2bd1db78d0f719ce8229a403a1e6099ddcae3ee7
Carolina Gonzalez <rpcgonzalez@gmail.com>
2020-12-14 21:05:37 -0500

    Final touches

--- script.js
+++ script.js
@@ -27,28 +27,39 @@ document.querySelector('.check').addEventListener('click', function() {
 			highscore = score;
 			document.querySelector('.highscore').textContent = highscore;
 		}
-	} else if (guess > number) {
-		//when the guess is too high
+	} else if (guess !== number) {
+		//When guess is wrong either too high or too low
 		if (score > 0) {
-			document.querySelector('.message').textContent = 'Guess is too high!';
-			score--;
-			document.querySelector('.score').textContent = score;
-		} else {
-			document.querySelector('.message').textContent = 'You lost!';
-		}
-	} else if (guess < number) {
-		//when the guess is too low
-
-		if (score > 0) {
-			document.querySelector('.message').textContent = 'Guess is too low!';
+			document.querySelector('.message').textContent = guess > number ? 'Guess is too high!' : 'Guess is too low';
 			score--;
 			document.querySelector('.score').textContent = score;
 		} else {
 			document.querySelector('.message').textContent = 'You lost!';
 		}
 	}
-	console.log(score);
-	//resetting the secretnumber, resetting to blank data entry number, reset score
+
+	// 	else if (guess > number) {
+	// 		//when the guess is too high
+	// 		if (score > 0) {
+	// 			document.querySelector('.message').textContent = 'Guess is too high!';
+	// 			score--;
+	// 			document.querySelector('.score').textContent = score;
+	// 		} else {
+	// 			document.querySelector('.message').textContent = 'You lost!';
+	// 		}
+	// 	} else if (guess < number) {
+	// 		//when the guess is too low
+
+	// 		if (score > 0) {
+	// 			document.querySelector('.message').textContent = 'Guess is too low!';
+	// 			score--;
+	// 			document.querySelector('.score').textContent = score;
+	// 		} else {
+	// 			document.querySelector('.message').textContent = 'You lost!';
+	// 		}
+	// 	}
+	// 	console.log(score);
+	// 	//resetting the secretnumber, resetting to blank data entry number, reset score
 });
 
 document.querySelector('.again').addEventListener('click', function() {
